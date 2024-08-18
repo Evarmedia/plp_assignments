f7cef2073c1c3a60d3cd34dd48708e09cae20e88
Evarmedia <mishakmanuel@gmail.com>
2024-08-18 15:14:32 +0100

    feat: week-9 assignment: working with lists and other date strutures

---
+++ week-9-python_assignment-evarmedia/week-9-assignment.py
@@ -0,0 +1,24 @@
+# Create an empty list called my_list.
+my_list = []
+
+firstList_to_be_added = [10, 20, 30, 40]
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+my_list.extend(firstList_to_be_added);
+
+# Insert the value 15 at the second position in the list.
+my_list[1] = 15
+
+# Extend my_list with another list: [50, 60, 70].
+another_list_to_be_added = [50, 60, 70]
+
+my_list.extend(another_list_to_be_added);
+
+# Remove the last element from my_list.
+my_list.pop();
+
+# Sort my_list in ascending order.
+my_list.sort();
+
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(30));
---
+++ week-9-python_assignment-evarmedia/weeklyChallenge.py
@@ -0,0 +1,71 @@
+''' Write a program that accepts user input to create a list of integers. Then, compute the sum of all the integers in the list. '''
+# userInput = input('Please enter integers, seperated by comma: ' )
+
+# inputList = userInput.split(',');
+
+# userList = [];
+
+# normal loop
+# for num in inputList: 
+#     userList.append(int(num))
+
+# userList = [ int(num) for num in inputList ]; looping with list compression
+
+# totalSum = sum(userList)
+
+# print(f' the Sum of the numbers you entered is: {totalSum}')
+
+''' Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.'''
+
+# myBooks = ('Lord of the Rings', 'the Hobbits', "Guiliver's Travel", "The time traveler's Wife"); 
+
+# printedBooks = [print(f'{book}') for book in myBooks]
+
+''' Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console. '''
+
+# user_data = {};
+
+# user_name = input('Please enter your name: ');
+# user_age = input('Please enter your age: ');
+# user_fav_color = input('Please enter your favorite color: ');
+
+# user_data["name"] = user_name;
+# user_data["age"] = user_age;
+# user_data["fav_color"] = user_fav_color;
+
+# print(f'User info in a dict is {user_data}');
+
+
+'''Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
+'''
+
+# set1 = set();
+# set2 = set();
+
+# user_input1 = input("Enter first set of integers, seperated by commas: ");
+# user_input2 = input("Enter second set of integers, seperated by commas: ");
+
+# user_list1 = user_input1.split(",");
+# user_list2 = user_input2.split(",");
+
+# set1.update(user_list1);
+# set2.update(user_list2);
+
+# intersection = set1 & set2;
+# print(f'Intersection of both set is: {intersection}');
+
+'''Create a program that stores a list of words. Then, use list comprehension to create a new list that contains only the words that have an odd number of characters.
+'''
+
+# words = [];
+
+# user_input = input('Enter list of words seperated by commas: ');
+
+# word_list = user_input.split(',');
+
+# print(word_list);
+
+# for word in word_list:
+#     stripped_word = word.strip();
+#     if len(stripped_word) % 2 == 1:
+#         print(stripped_word);
